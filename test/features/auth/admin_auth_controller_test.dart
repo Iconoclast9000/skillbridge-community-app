3626ea32474c8d5d77c33a0bbfd487c1011a28c7
Craig9000 <93838897+Iconoclast9000@users.noreply.github.com>
2024-12-18 18:58:42 +0200

    Fix shared_preferences import in admin_auth_controller_test.dart

--- test/features/auth/admin_auth_controller_test.dart
+++ test/features/auth/admin_auth_controller_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:skillbridge_community_app/features/auth/admin_auth_controller.dart';
 
 void main() {
