2402d3549f3cb3712c318ea90ef0f587cf96f33c
Craig9000 <93838897+Iconoclast9000@users.noreply.github.com>
2024-12-18 19:07:31 +0200

    Fix widget test to use SkillBridgeApp instead of MyApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:skillbridge_community_app/main.dart';
 
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-    expect(find.text('Welcome to SkillBridge'), findsOneWidget);
+    SharedPreferences.setMockInitialValues({});
+    final prefs = await SharedPreferences.getInstance();
+    
+    await tester.pumpWidget(SkillBridgeApp(prefs: prefs));
+    expect(find.text('Login'), findsOneWidget);
   });
 }
