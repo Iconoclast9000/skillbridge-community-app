7b6d85fda0419dc5173019c009e6367bd709129c
Craig9000 <93838897+Iconoclast9000@users.noreply.github.com>
2024-12-18 19:00:15 +0200

    Fix admin_login_screen_test.dart format

--- test/screens/admin/admin_login_screen_test.dart
+++ test/screens/admin/admin_login_screen_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:skillbridge_community_app/features/auth/admin_auth_controller.dart';
 import 'package:skillbridge_community_app/screens/admin/admin_login_screen.dart';
 
